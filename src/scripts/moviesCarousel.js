9a1ea52874b6e61918ce2fb955f04a31c6e72405
Fabiojcp <fabio50019@gmail.com>
2023-01-29 21:03:00 -0300

    feat: movies carousel, user carousel initiated

---
+++ src/scripts/moviesCarousel.js
@@ -0,0 +1,49 @@
+const images = [
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/banker.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/batman.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/bruxo.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/homemnorte.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/joker.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/loki.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/mandalorian.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/strange.jpeg" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/tim.png" />
+</li>`,
+  `<li class="movieSlide animate">
+  <img src="./src/assets/MoviesCarousel/vikings.jpeg" />
+</li>`,
+];
+
+const animations = [];
+document.addEventListener("DOMContentLoaded", () => {
+  let actualValue = 0;
+  const moviesCarousel = document.getElementById("moviesCarousel");
+  const childs = document.getElementsByClassName("movieSlide");
+
+  setInterval(() => {
+    moviesCarousel.removeChild(childs[0]);
+
+    moviesCarousel.innerHTML += images[actualValue];
+    actualValue++;
+    if (actualValue > 9) {
+      actualValue = 0;
+    }
+  }, 4000);
+});
