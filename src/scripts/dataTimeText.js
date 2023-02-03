416669ec0fd0365b86ec4d6515809f077a66e6d9
Gabriel <gabriellf.dev@gmail.com>
2023-02-03 11:47:48 -0300

    att-03/02

--- src/scripts/dataTimeText.js
+++
@@ -1,3 +0,0 @@
-let date = new Date();
-let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-document.getElementById("today").innerHTML = "ATENÇÃO: Este produto está prestes a se esgotar, oferta especial apenas hoje! " + date.toLocaleDateString('pt-BR', options);
